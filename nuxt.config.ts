8a6898e8b27286a4ef283b1b499ba89893dc44e5
Josué  Rufino <73407358+JosueRufino@users.noreply.github.com>
2024-11-19 01:49:49 +0100

    construindo layout de login

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,29 @@
 export default defineNuxtConfig({
   compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
+  app: {
+    head: {
+      title: "Software JJGB",
+      script: [
+        {
+          defer: true,
+          tagPosition: "bodyClose",
+          src: "https://cdn.jsdelivr.net/npm/bootstrap@5/dist/js/bootstrap.bundle.min.js",
+        },
+      ],
+      link: [
+        {
+          rel: "icon",
+          type: "image/png",
+          href: "/assets/puzzle.png",
+        },
+      ],
+      meta: [{ charset: "utf-8" }],
+    },
+  },
   modules: ["@pinia/nuxt"],
+  css: [
+    "bootstrap/dist/css/bootstrap.min.css",
+    "bootstrap-icons/font/bootstrap-icons.css",
+  ],
 });
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,63 @@
 <template>
-    <div>
-      <h1>Welcome to the JJGB LIBRARY</h1>
-      <h1>Genilson</h1>
+  <div class="containe">
+    <div class="content-img">
+      <div class="overlay"></div>
+      <div class="content d-flex justify-content-center">
+        <div class="me-4">
+          <h1>BIBLIOTECA JJGB</h1>
+          <p>Bem-vindo ao sistema</p>
+        </div>
+        <div class="card mt" style="width: 400px; height: 70vh;">jjj</div>
+      </div>
     </div>
-  </template>
+  </div>
+</template>
+
+<script setup></script>
+
+<style scoped>
+* {
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+}
+
+.container {
+  width: 100%;
+  min-height: 100vh;
+}
+
+.content-img {
+  height: 300px;
+  width: 100%;
+  position: relative;
+  background-image: url("../assets/image.jpeg");
+  background-size: cover;
+  background-position: center;
+}
+
+.overlay {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(37, 150, 190, 0.6);
+}
+
+.content {
+  position: relative;
+  z-index: 1;
+  padding: 2rem;
+  color: white;
+}
+
+h1 {
+  font-size: 2rem;
+  margin-bottom: 0.5rem;
+}
+
+p {
+  font-size: 1.1rem;
+}
+</style>
