e9c604898b3c531acca24540f616815e9cb850be
Josué  Rufino <73407358+JosueRufino@users.noreply.github.com>
2024-12-04 11:16:28 +0100

    adicionada nova formatação de design nos cards de emprestimo

--- components/livros/cardEL.vue
+++ components/livros/cardEL.vue
@@ -2,29 +2,41 @@
   <div>
     <LivrosModalEmprestarLivro :book="book" />
     <LivrosModelAdicionarListaEspera />
-    <div class="card border-0 p-0" style="width: 12rem">
+    <div class="card border-0 p-0" style="width: 11rem">
       <div
-        class="w-50 p-1 position-absolute text-white text-center"
+        class="status-label w-50 p-1 position-absolute text-white text-center"
         :class="book.status ? 'bg-success' : 'bg-danger'"
+        style="font-size: 0.8rem"
       >
         {{ book.status ? "Disponível" : "Indisponível" }}
       </div>
       <div class="p-3">
-        <img
-          :src="book.imagem ? book.imagem : '/assets/book3.png'"
-          style="max-height: 200px; width: 100%; background-size: cover"
-          alt="..."
-        />
+        <div style="height: 30vh">
+          <img
+            v-if="book.imagem"
+            :src="book.imagem"
+            style="background-size: cover; width: 100%"
+            alt="..."
+          />
+          <img
+            v-else
+            src="/assets/book.png"
+            style="background-size: cover; width: 100%"
+            alt="..."
+          />
+        </div>
         <div class="mt-2">
-          <h5 class="card-title">{{ book.titulo }}</h5>
-          <p class="text-secondary mb-1">Autor: {{ book.autor }}</p>
+          <h5 class="card-title text-truncate">{{ book.titulo }}</h5>
+          <p class="text-secondary mb-1 author-text" style="font-size: 0.8rem">
+            Autor: {{ book.autor }}
+          </p>
         </div>
         <div class="d-flex justify-content-between mt-2 w-100">
           <!-- Botão para abrir o modal e atualizar a rota -->
           <button
             v-if="book.status"
             type="button"
-            class="btn btn-success"
+            class="btn btn-success btn-sm"
             data-bs-toggle="modal"
             data-bs-target="#emprestimoModal"
             @click="addRouteParameter(book.id)"
@@ -35,12 +47,12 @@
           <button
             v-else
             type="button"
-            class="btn btn-danger"
+            class="btn btn-danger btn-sm"
             data-bs-toggle="modal"
             data-bs-target="#listaEsperaModal"
             @click="addRouteParameter(book.id)"
           >
-            Adicionar na fila de espera
+            Colocar em  espera
           </button>
         </div>
       </div>
@@ -82,5 +94,17 @@ const useBook = useBookStore();
 .card:hover {
   box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;
   transform: scale(1.05);
+}
+
+.card-title {
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+}
+
+.author-text {
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
 }
 </style>
